8c7566d3489a3d7be12922812c5b525528d79738
Binh <thanhbinh12b4lh2022@gmail.com>
2024-12-25 14:43:12 +0700

    new

---
+++ FEBC75/src/routes/index.tsx
@@ -0,0 +1,46 @@
+import HomeTemplate from "../pages/HomeTemplate";
+import HomePage from "../pages/HomeTemplate";
+import { Route } from "react-router-dom";
+
+type TRoute = {
+  path: string;
+  element: () => React.ReactNode;
+  children?: TRoute[];
+};
+
+const routes: TRoute[] = [
+  {
+    path: "",
+    element: HomeTemplate,
+    children: [
+      {
+        path: "",
+        element: HomePage,
+      },
+    ],
+  },
+];
+
+const renderRoutes = () => {
+  return routes.map((route) => {
+    if (route.children) {
+      return (
+        <Route key={route.path} path={route.path} element={<route.element />}>
+          {route.children.map((item) => (
+            <Route
+              key={item.path}
+              path={item.path}
+              element={<item.element />}
+            />
+          ))}
+        </Route>
+      );
+    } else {
+      return (
+        <Route key={route.path} path={route.path} element={<route.element />} />
+      );
+    }
+  });
+};
+
+export default renderRoutes;
