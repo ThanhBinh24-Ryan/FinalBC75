f20a0c7088b06a6a69331ba499495c4db52a282e
jack20492 <nphuc210@gmail.com>
2025-01-17 08:54:40 +0700

    phuc up lan 1

---
+++ FEBC75/src/hooks/search-hook.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { searchApi } from "../apis/search-api";
+
+export const useSearch = (tenCongViec: string) => {
+  return useQuery({
+    queryKey: ["search", tenCongViec],
+    queryFn: () => searchApi(tenCongViec),
+  });
+};
